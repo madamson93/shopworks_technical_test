e96b8057bb6bf7c7840de68420785837dd332f53
Monique Adamson <madamson@hotmail.co.uk>
2018-03-14 18:19:26 +0000

    Create controller for functionality
    Query the database using the model and repository, render a view with the data

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Model\Shift;
+use App\Repositories\ShiftRepository;
+
 
 class ShiftController extends Controller
 {
+	// variable to point to model for repository
+	protected $model;
+
+
+	/**
+	 * ShiftController constructor.
+	 * @param Shift $shift
+	 */
+	public function __construct(Shift $shift)
+	{
+		$this->model = new ShiftRepository($shift);
+	}
+
+
 	public function index()
 	{
+		$allShifts = [];
 
+		for($i = 0; $i <= 60; $i++) {
+			$allShifts[] = $this->model->findShiftsByStaffID((int) $i);
+		}
+
+		return view('index')->with('shifts', $allShifts);
     }
 }
